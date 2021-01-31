9f90134349a482eed2fb69a3642a5211bfa199b2
raulynas <laurynas@inwoodengineering.co.uk>
2021-01-31 12:48:37 +0100

    Initial commit

---
+++ js/components/clock.js
@@ -0,0 +1,15 @@
+function clock(selector) {
+    const DOM = document.querySelector(selector);
+    let number = parseInt(DOM.innerText);
+
+const timer = setInterval(function () {
+    DOM.innerText = number--;
+    if (number < 0){
+        DOM.innerText = "Finito";
+        clearInterval(timer);
+    }
+}, 1000)
+}
+
+
+export {clock}
